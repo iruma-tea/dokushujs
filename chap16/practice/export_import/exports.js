c8977f4d64fabc70a93279ebe02d2a62afa8d4a9
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-11-13 13:54:24 +0900

    16.3 インポートまで学習完了

---
+++ chap16/practice/export_import/exports.js
@@ -0,0 +1,21 @@
+/* 回答① */
+export const obj = {
+    console() {
+        console.log("オブジェクト（obj）");
+    },
+};
+
+// 関数（fn）のエクスポート
+export function fn() {
+    console.log("関数（fn）");
+  }
+// クラス（StdClass）のエクスポート
+export class StdClass {
+    static console() {
+        console.log("クラス（StdClass）");
+    }
+}
+// 関数をデフォルトエクスポート
+export default function () {
+    console.log("デフォルトエクスポート関数");
+}
