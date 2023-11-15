abcf350f1f759fb1b294c99ece5391f35cf06c89
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-11-15 21:12:14 +0900

    chap16の学習完了

---
+++ chap16/cjs_export.js
@@ -0,0 +1,8 @@
+// module.exportsとexportsに同じオブジェクトを設定する
+module.exports = exports = {
+    exportedFunction: function () {},
+};
+exports.exportedVariable = "これはエクスポートされます。";
+console.log(module.exports);
+// > { exportedFunction: function () {}, exportedVariable: "これはエクスポートされます。" }
+  
